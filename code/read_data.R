fc2ee69a9431ffa142e0509a9c70db402504ce94
paulse1 <p.seitz@campus.lmu.de>
2025-02-26 16:52:33 +0100

    code.R : reading in data and pivoting wider

--- code/read_data.R
+++ code/read_data.R
@@ -1,1 +1,15 @@
+## Read CSV
+
 data <- read_csv("data/raw/1997-01-01-2025-01-01-Nigeria.csv")
+
+## Each conflict is identified by an ID (event_id_cnty)
+## to make data "tidy" each observation needs to be one row, so the data needs to be pivoted wider
+## before that one has to create a column to take names from though
+
+widened.data <- data |>
+  group_by(event_id_cnty) |>
+  mutate(actor = paste0("actor", 1:length(event_id_cnty))) |>
+  ungroup() |> 
+  pivot_wider(names_from = actor, values_from = actor1)
+
+saveRDS(widened.data, file = "data/intermediate/wide_data.RDS")
