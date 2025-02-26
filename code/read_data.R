52b9a38f7a12d843c9a8982cc1c27450105b4984
paulse1 <p.seitz@campus.lmu.de>
2025-02-26 18:17:16 +0100

    small improvement

--- code/read_data.R
+++ code/read_data.R
@@ -8,9 +8,8 @@ data <- read_csv("data/raw/1997-01-01-2025-01-01-Nigeria.csv")
 
 widened.data <- data |>
   group_by(event_id_cnty) |>
-  mutate(actor = paste0("actor", 1:length(event_id_cnty))) |>
+  mutate(actor = paste0("actor", seq_len(length(event_id_cnty)))) |>
   ungroup() |> 
   pivot_wider(names_from = actor, values_from = actor1)
 
 saveRDS(widened.data, file = "data/intermediate/wide_data.RDS")
-
