e48e8a39372f4e10b6a9543ab87db236791fe46c
Shuangying Xu <XuShu@sbsit.com>
2025-02-28 21:30:27 +0100

    update workplan

---
+++ code/Notes.R
@@ -0,0 +1,49 @@
+# ### Analysis questions
+
+# ## Is there an association between type of conflict and conflict groups? 
+# 1. start from event_type/sub_event_type + actor1/2 -> who is always doing what?
+# 2. add more column? -> additional info: fatality / population
+  
+# ## Is there a change in the type of conflict observable over time?
+# 1. time?
+# 2. event_type/sub_event_type -> tendency?
+
+# ## Geologie?
+# 1. time + geo the same == the same event, but different story
+
+# ## Source? a part of human behavior
+# 1. international / national / local -> external / internal inflence on facts
+
+# ### Data process
+# ## raw data in general 
+# 1. date format, time process based on timestamp
+# 2. actor -> actor1+ actor2 wider: exceptions handling col by col check
+# 3. notes structure definition, check if needed
+
+# ## proposals related
+# 1.
+# 2. 
+# 3. 
+
+# ### Visualization concepts, outputs?
+# ## how to visualize each proposals
+# 1.
+# 2. 
+# 3. 
+
+# ## what conclusion?
+# 1.
+# 2. 
+# 3. 
+
+# ### Programming
+# ## R frameworks
+# ## utilities: 
+# 1. data proecss
+# 2. analysis
+# 3. visualization
+# 
+
+
+
+
