4c176aacdea218d581125867e287310c6269d3a7
Marcsman <Marc.landes@campus.lmu.de>
2025-02-28 11:47:46 +0100

    fixed bool creation for civilians targeted and cleared out all quotation marks

--- code/read_data.R
+++ code/read_data.R
@@ -7,7 +7,7 @@ data <- read_csv("data/raw/1997-01-01-2025-01-01-Nigeria.csv")
 ## before that one has to create a column to take names from though
 
 widened.data <- data |>
-  mutate(civilian_targeting = !is.na(civilian_targeting)) |>
+  mutate(civilian_targeting = ifelse(replace_na(civilian_targeting, "") == "Civilian targeting", TRUE, FALSE)) |>
   group_by(event_id_cnty) |>
   mutate(actor = paste0("actor", seq_len(length(event_id_cnty)))) |>
   ungroup() |> 
