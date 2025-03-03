cbee12e579630d0ec7ae4de5a18ba8de3971666e
paulse1 <p.seitz@campus.lmu.de>
2025-03-03 10:00:28 +0100

    some change to code but doesnt really work

--- code/read_data.R
+++ code/read_data.R
@@ -15,22 +15,21 @@ widened.data <- data |>
 
 saveRDS(widened.data, file = "data/intermediate/wide_data.RDS")
 
-##Function to sub " **month** " with "**/monthnumber/**" in character vectors
-##as preparation for as_date conversion
+##supposed to be a function with the same use as parse_date, but does not work
 
-month_sub_function <- function(x) {
+as_date_function <- function(x) {
   assertCharacter(x)
   months <- c(January = 1, February = 2, March = 3, April = 4, May = 5,
               June = 6, July = 7, August = 8, September = 9,
               October = 10, November = 11, December = 12)
-  for (m in names(months)) {
-    x <- gsub(paste0(" ", m," "), paste0("/", months[m], "/"), x)
-  }
+  marker <- str_detect(x, paste0("(", paste(names(months), collapse = "|"), ")"))
+  x[marker] <- as_date(x, format = "%e %B %Y")
+  x[!marker] <- as_date(x, format = "%m/%d/%Y")
   x
 }
 
-## Categorization of Militant Groups changing longitude and latitude var name
-## Also "doubled" Dates
+## Categorization of Militant Groups, changing longitude and latitude var name,
+## using as_date_function to transform dates
 
 categorized_data <- widened.data |>
   mutate(group_association = case_when(str_detect(actor1, "Police") ~ "Police",
@@ -44,6 +43,6 @@ categorized_data <- widened.data |>
                ) |> 
   mutate(group_association = ifelse(is.na(group_association), "Other", group_association)) |> 
   rename(long = longitude, lat = latitude) |> 
-  mutate(event_date = as_date(month_sub_function(event_date), format = "%m/%d/%Y"))
+  mutate(event_date = as_date_function(event_date))
 
 
