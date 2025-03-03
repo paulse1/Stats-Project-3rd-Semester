224de39180700d6bd75df382aca370e0351191b6
Marcsman <Marc.landes@campus.lmu.de>
2025-03-03 10:03:20 +0100

    population_best changes

--- code/read_data.R
+++ code/read_data.R
@@ -1,7 +1,6 @@
 ## Read CSV
 
 data <- read_csv("data/raw/1997-01-01-2025-01-01-Nigeria.csv")
-data1<- read_csv("data/raw/1997-01-01-2025-01-01-Nigeria.csv")
 
 ## Each conflict is identified by an ID (event_id_cnty)
 ## to make data "tidy" each observation needs to be one row, so the data needs to be pivoted wider
@@ -12,13 +11,15 @@ clean.data <- data |>
   mutate(population_best = str_extract(population_best, "[0-9]+,*",)) |>
   mutate(across(everything(), ~ gsub(',{2,}', "", .))) |>
   group_by(event_id_cnty) |>
-  mutate(fatalities = ifelse(is.na(fatalities), first(as.numeric(na.omit(fatalities))), as.numeric(fatalities))) |>
-  mutate(timestamp = ifelse(is.na(timestamp), first(as.numeric(na.omit(timestamp))), as.numeric(timestamp))) |>
+  mutate(fatalities = ifelse(is.na(fatalities), as.numeric(first(na.omit(fatalities))), as.numeric(fatalities))) |>
+  mutate(timestamp = ifelse(is.na(timestamp), as.numeric(first(na.omit(timestamp))), as.numeric(timestamp))) |>
   mutate(notes = notes[which.max(nchar(notes))]) |>
   ungroup() |>
   group_by(year, location) |>
-  mutate(population_best = first(na.omit(population_best))) |>
+  mutate(population_best = as.numeric(first(na.omit(population_best)))) |>
   ungroup()
+
+saveRDS(clean.data, file = "data/intermediate/clean_data.RDS")
   
   
 widened.data <- clean.data |>
