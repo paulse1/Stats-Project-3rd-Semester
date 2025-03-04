47a8fade8c07e62d86144e4e3d009d659eca1a5a
Shuangying Xu <XuShu@sbsit.com>
2025-03-05 00:40:46 +0100

    update report.md

---
+++ code/AnalysisClean.R
@@ -0,0 +1,296 @@
+
+# =================================
+# 1 Single variables
+# =================================
+
+# ---------------------------------
+# 1_1 event type - sub event type
+# ---------------------------------
+
+### use wide data to make event unique
+
+
+
+
+# proportion of event types 
+# pie chart
+
+
+
+
+# compare counts of sub event types 
+# Stacked Bar Chart of types and sub types
+
+
+
+
+# ---------------------------------
+# 1_2 about actor
+# ---------------------------------
+
+### use long data to fully count actors
+
+# detect most active actors: top 10 / top 20
+# bar charts
+
+
+
+
+
+# to be noticed: top 5+ are civilians or official groups
+# to get insight of rebellious armed group using filter
+# bar charts: top 10, using color to represent if Nigeria
+
+
+
+
+
+
+### as 439 different actors are beyond understandable
+### categorize actors in 7 groups acc. inter. code (doc from ACLED)
+### incomplete, need to be validated
+
+
+
+
+
+
+# compare counts of categories 
+# as incomplete categorization, do 2 versions of with / without uncategorized 
+# bar chart
+
+
+
+
+
+# =================================
+# 2 Time Series Analysis
+# =================================
+
+# ---------------------------------
+# 2_1 series analysis of event type
+# ---------------------------------
+
+### use wide data to make event unique
+### ???: the value of 2025 be filtered out as incomplete statistics
+
+
+
+
+
+# to explore the trend of total number of events
+# line chart: year - count of event type (sum)
+
+
+
+
+# to explore the trend of each type of event
+# line chart: year - count of each event type, grouped by event type
+# colored by different event types
+
+
+
+
+# to explore the seasonal periodicity of total number of events 
+# line chart: month - count of event type (sum)
+
+
+
+
+# to explore the seasonal periodicity of each type of event
+# line chart: month - count of each event type, grouped by event type
+# colored by different event types
+
+
+
+
+
+# ---------------------------------
+# 2_2 series analysis of actor
+# ---------------------------------
+
+### use long data to fully count actors
+### use the categorized actor data
+
+
+
+
+# to explore the trend of number of events caused by top 10
+# line chart: year - count of event, grouped by actor
+# colored by different actor
+
+
+
+
+# to explore the seasonal periodicity of number of events caused by top 10
+# line chart: month - count of event, grouped by actor
+# colored by different actor
+
+
+
+
+# to explore the trend of number of events caused by actor category
+# line chart: year - count of event, grouped by actor category
+# colored by different actor category
+
+
+
+
+# to explore the seasonal periodicity of number of events caused by actor category
+# line chart: month - count of event, grouped by actor category
+# colored by different actor category
+
+
+
+
+
+# =================================
+# 3 Geographical distributions 
+# =================================
+
+### get map data for Nigeria
+### ??? think of getting terrain data, such as contour lines
+### one way could be using Google map by ggmap(), but API key required
+
+
+
+
+
+# ---------------------------------
+# 3_1 Overview of distribution
+# ---------------------------------
+
+### use wide data to make event unique
+
+
+
+
+# to explore the distribution of total number of events
+# map plot with all event points with transparency (alpha = 0.1)
+
+
+
+
+
+# alternatively making a geological heat map (bins)
+
+
+
+
+# ---------------------------------
+# 3_2 Distribution of event type
+# ---------------------------------
+
+### use wide data to make event unique
+
+
+
+
+# to explore the distribution of each type of event
+# faceted map plot charts: with each event type a subplot, grouped by event type
+# colored by different event types
+
+
+
+
+
+# ---------------------------------
+# 3_3 Distribution of actors
+# ---------------------------------
+
+### use long data to fully count actors
+### use the categorized actor data
+
+
+
+# to explore the distribution of each top 10 actor
+# faceted map plot charts: with each event type a subplot, grouped by actor
+# colored by different actors
+
+
+
+
+# to explore the distribution of each actor category
+# faceted map plot charts: with each event type a subplot, grouped by actor category
+# colored by different actor categories
+
+
+
+
+
+
+# =================================
+# 4 Association Analysis 
+# =================================
+
+# ---------------------------------
+# 4_1 Casualty Analysis
+# ---------------------------------
+
+### use wide data to make event unique
+
+
+
+# study on average / median / sum of fatalities per event type
+# ??? which key value is the best
+# bar chart
+
+
+
+
+
+# alternatively study on event type and sub event type
+# therefore stacked bar chart
+
+
+
+
+# more alternatively: study on population influenced?
+
+
+
+
+
+# ---------------------------------
+# 4_2 Event types vs actor categories
+# ---------------------------------
+
+# Chi-Square Test
+
+
+# Cramer’s V Coefficient
+
+
+
+
+
+
+# ---------------------------------
+# 4_3 Time vs Geography
+# ---------------------------------
+
+
+
+
+# =================================
+# 5 Advanced Analysis 
+# =================================
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
