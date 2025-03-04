0f497a260a12a511416f1e4a9b0b43a1df09ae08
Shuangying Xu <XuShu@sbsit.com>
2025-03-04 20:23:56 +0100

    update plot tasks

---
+++ code/AnalysisClean.R
@@ -0,0 +1,74 @@
+
+# =================================
+# 1 Single variables
+# =================================
+
+# ---------------------------------
+# 1_1 event type - sub event type
+# ---------------------------------
+
+# use wide data to make event unique
+
+
+
+
+# proportion of event types 
+# pie chart
+
+
+
+
+# compare counts of sub event types 
+# Stacked Bar Chart of types and sub types
+
+
+
+
+# ---------------------------------
+# 1_2 about actor
+# ---------------------------------
+
+# use long data to fully count actors
+
+# detect most active actors: top 10 / top 20
+# bar charts
+
+
+
+
+
+# to be noticed: top 5+ are civilians or official groups
+# to get insight of rebellious armed group using filter
+# bar charts: top 10, using color to represent if Nigeria
+
+
+
+
+
+
+# as 439 different actors are beyond understandable
+# categorize actors in 7 groups acc. inter. code (doc from ACLED)
+# incomplete, need to be validated
+
+
+
+
+
+
+# compare counts of categories 
+# as incomplete categorization, do 2 versions of with / without uncategorized 
+# bar chart
+
+
+
+
+
+
+
+
+
+
+
+
+
+
